aa7579f181d126c7dd0bd560e9b481be2bc77253
Noah Snelson <noah.snelson@protonmail.com>
2021-12-01 20:30:23 -0800

    Change username rule to be `email` rather than just `string`

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,7 +11,7 @@ class LoginController extends Controller
     public function loginUser(Request $request)
     {
         $creds = $request->validate([
-            'username' => 'string|required',
+            'username' => 'email|required',
             'password' => 'string|required',
         ]);
 
