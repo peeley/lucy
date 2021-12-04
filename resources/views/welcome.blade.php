34daf4999ae3bf87ddac4b2ee2f2e6937acd92fd
lvs-vong <64504438+lvs-vong@users.noreply.github.com>
2021-12-03 22:08:52 -0800

    Added the redirects on the buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,20 +4,22 @@
 
 <body style="background-color:lightslategray">
 <center>
-    <h1>
+    <h1 style = "padding-top: 30px">
         <p style="font-size:2vw">Welcome to</p>
         <p style="font-size:3vw">Project Lucy</p>
     </h1>
+    <p>An Adaptive Alternative Communication Open Source Solution</p>
     <h1>
         <button style="width: 200px; height: 80px; background-color:lightgoldenrodyellow;">
-        <font size="+1"><a href="http://localhost:8080">Log in</a></font>
+        <font size="+1"><a href="http://localhost:8080/login">Log in</a></font>
         </button>
         <button style="width: 200px; height: 80px; background-color:lightgoldenrodyellow;">
-        <font size="+1"><a href="http://localhost:8080">Create Account</a></font>
+        <font size="+1"><a href="http://localhost:8080/create-account">Create Account</a></font>
         </button>
         <button style="width: 200px; height: 80px; background-color:lightgoldenrodyellow;">
         <font size="+1"><a href="http://localhost:8080">Guest</a></font>
         </button>
     </h1>
+    
 </center>
 </html>
