0b005cab971e2362e02ab16f2dfec75debf94582
lvs-vong <64504438+lvs-vong@users.noreply.github.com>
2021-12-03 20:56:56 -0800

    changed sizes of texts and added color

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,7 @@
 <html>
 
-  <body>
+  <body style = "background-color:lightslategray">
+  <h1 style="padding: 20px"><button style = "width: 200px; height: 100px; background-color:lightgoldenrodyellow;"> <font size = "+2">Previous Screen</font></button></h1>
   <center>
     <form action="/login" method="POST">
       @csrf
@@ -9,9 +10,9 @@
       <div>{{ $message }}</div>
       @enderror
       
-      <h1><input type="text" name="email" placeholder="Email" /></h1>
-      <h1><input type="password" name="password" placeholder="Password" /></h1>
-      <h1><button type="submit">Submit</button></h1>
+      <h1><input type="text" name="email" placeholder="Email Address"  style="background-color:lightgoldenrodyellow; width: 400px; height: 50px;"/></h1>
+      <h1><input type="password" name="password" placeholder="Password" style="background-color:lightgoldenrodyellow; width: 400px; height: 50px;"/></h1>
+      <h1><button type="submit" style="width: 200px; height: 50px;background-color:lightgoldenrodyellow;"> <font size = "+2">Log in </font></button></h1>
     </form>
   </center>
   </body>
