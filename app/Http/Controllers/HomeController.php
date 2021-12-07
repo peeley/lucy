21682e303e9be688d07859740d8d89e6e58c5086
Noah Snelson <noah.snelson@protonmail.com>
2021-12-06 22:22:47 -0800

    Convert home screen to list of boards

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,11 @@ class HomeController extends Controller
     {
         $user = $request->user();
 
-        return view('home', ['name' => $user->name]);
+        $boards = [(object) ['id' => 1, 'name' => 'Default board']];
+
+        return view('home', [
+            'name' => $user->name,
+            'boards' => $boards
+        ]);
     }
 }
