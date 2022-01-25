70575ecac648c7f068348de6293de439ec90e4c9
lvs-vong <64504438+lvs-vong@users.noreply.github.com>
2022-01-24 19:08:52 -0800

    Added some basic CSS

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,45 +1,51 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>About Us</title>
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/about-us.css">
 </head>
-<body>
-    <div>
-        <div>
-            <h1>CS 426 Senior Project</h1>
-            <h2>Project Lucy</h2>
-            <h4>Computer Science and Engineering</h4>
-            <h4>University of Nevada, Reno</h4>
-        </div>
-        <h4>Team 25: Eloisa Burton, Elizabeth Schuon, Elvis Vong, Noah Snelson, Mehar Mangat</h4>
 
-        <h4>Teaching Team: David Feil-Seifer (UNR), Devrin Lee (UNR), Vinh Le (UNR), Yifan Zhang (UNR)</h4>
-        <h4>Advisor: Emily Hand (UNR)
-
+<body class="about-body">
+    <div class="about-header">
+        <h1>CS 426 Senior Project</h1>
+        <h1>Project Lucy</h1>
+        <h3>
+            Computer Science and Engineering <br>
+            University of Nevada, Reno
+        </h3>
+        <h4>
+            Team 25: <i>Eloisa Burton, Elizabeth Schuon, Elvis Vong, Noah Snelson, Mehar Mangat</i> <br>
+            Teaching Team: <i>David Feil-Seifer (UNR), Devrin Lee (UNR), Vinh Le (UNR), Yifan Zhang (UNR)</i> <br>
+            Advisor: <i>Emily Hand (UNR)</i> 
+        </h4>
     </div>
-    <div>
+    <div class="about-main">
+        <h3><b>About Project Lucy</b></h3>
         <p>
-        Solutions for augmentative and alternative communication (AAC) for non-verbal individuals are often expensive and difficult to use. 
-        Many current AAC applications are priced upwards of $200 on the mobile app market in addition to the cost of the tablet; 
-        these costs, while necessary to fund development, may be prohibitively expensive for individuals who need these apps to communicate.
-        On the other hand, free AAC applications do exist, but they limit the amount of vocabulary the user can use,
-        or there is a maximum word limit per sentence. The users can only get rid of these restrictions when they upgrade to the premium edition which can be very costly.
-        </p>
-        <p>
-            Project Lucy will be an affordable, open-source, and cross-platform augmentative and alternative communication (AAC) app that allows users to communicate non-verbally. 
+            Solutions for augmentative and alternative communication (AAC) for non-verbal individuals are often expensive and difficult to use.
+            Many current AAC applications are priced upwards of $200 on the mobile app market in addition to the cost of the tablet;
+            these costs, while necessary to fund development, may be prohibitively expensive for individuals who need these apps to communicate.
+            On the other hand, free AAC applications do exist, but they limit the amount of vocabulary the user can use,
+            or there is a maximum word limit per sentence. The users can only get rid of these restrictions when they upgrade to the premium edition which can be very costly.
+            <br> <br>
+            Project Lucy will be an affordable, open-source, and cross-platform augmentative and alternative communication (AAC) app that allows users to communicate non-verbally.
             The application will allow users to construct sentences from a grid of tiles representing words and phrases accompanied by illustrative icons, which the application will then read aloud for nearby persons to hear.
-            Project Lucy would also have an array of customization features to increase the usability of the program. 
-            Things such as changing the color of tiles or adding custom icons to be displayed. 
-            This will all be integrated with the ability to make custom tiles. 
-            These tiles could be a single word, a phrase, or a folder that contains other words, phrases, or folders. 
+            Project Lucy would also have an array of customization features to increase the usability of the program.
+            Things such as changing the color of tiles or adding custom icons to be displayed.
+            This will all be integrated with the ability to make custom tiles.
+            These tiles could be a single word, a phrase, or a folder that contains other words, phrases, or folders.
             Additionally, the application would feature guided use that instructs users on how to use the application rather than going through tutorials or memorizing functionality
         </p>
-    </div>
-    <a href="http://localhost:8080/">
-            <button class = "welcome-button">Go to Project Lucy</button>
-    <div>
+
+        <a href="http://localhost:8080/">
+            <button class="welcome-button">Go to Project Lucy</button>
+        </a>
+
         <h3>Project Related Resources</h3>
+        
     </div>
 </body>
+
 </html>
