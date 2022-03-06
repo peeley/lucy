cfb030b41babc1e5a21e275c281ee7b5f227d307
Noah Snelson <noah.snelson@protonmail.com>
2022-03-05 16:46:09 -0800

    Remove `ClickAndHold` import from `app.js`

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ require('./bootstrap');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Board } from './components/Board.js';
-import ClickAndHold from './ClickAndHold';
 
 // mount the React app to the <div id="root"> tag
 let rootElement = document.getElementById('root');
