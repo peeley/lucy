32b3942cee4f5704024e28258bfc961d7afa15d6
lvs-vong <64504438+lvs-vong@users.noreply.github.com>
2022-03-07 21:24:00 -0800

    Merge branch 'master' of https://github.com/peeley/lucy into cleaning-css

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -1,9 +1,4 @@
 <html>
-  <h2 style="padding: 20px">
-    <form>
-      <button class="back-button" formaction="/">Back</button>
-    </form>
-  </h2>
   <div id="root" board_id={{ $board_id }}></div>
   <link rel="stylesheet" href="/css/app.css">
   <script src="/js/app.js"></script>
