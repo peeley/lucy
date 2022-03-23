2560668e2392148e74df8e7b65bee648da04d5da
Mehar Mangat <mmangat@nevada.unr.edu>
2022-03-22 17:54:14 -0700

    Added css stylings for the home view, light and dark themes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,35 +2,41 @@
   <head>
     <title>Lucy - Home</title>
       @include('layouts.includes.header')
+      <link rel="stylesheet" href="@if(session('isDark')) /css/home-dark.css @else /css/home.css @endif " />
   </head>
   <body>
   @include('layouts.includes.darkThemeToggle')
-  <p>You are logged in as {{ $name }}.</p>
-    <a href="/logout">Logout</a>
-    <a href="/user-settings">Settings</a>
+
+
+  <p class="home-welcome-title">Welcome, {{ $name }}.</p>
+  <ul class="home-bar">
+    <li><a href="/logout">Logout</a></li>
+     <li><a href="/user-settings">Settings</a></li>
+  </ul>
 
     <form action="/boards" method="POST">
       @csrf
-      <button type="submit">Create Board</button>
+      <button class="home-create-board-button" type="submit">Create Board</button>
     </form>
-    <ul>
+    <ul class="home-boards">
     @foreach ($boards as $board)
-      <li>
+      <li class="home-board">
         <a href="/boards/{{ $board->id }}"> {{ $board->name }} </a>
-        <form action="/boards/{{ $board->id }}" method="POST">
+          <button onclick="document.getElementById('modal-{{ $board->id }}').style.display='block'"> Edit </button>
+
+          <form action="/boards/{{ $board->id }}" method="POST">
           @method('DELETE')
           @csrf
           <button type="submit">Delete</button>
         </form>
-        <button onclick="document.getElementById('modal-{{ $board->id }}').style.display='block'"> Edit </button>
         <div id="modal-{{ $board->id }}" style="display: none;">
           <button onclick="document.getElementById('modal-{{ $board->id }}').style.display='none'">Close &times;</button>
           <form action="/boards/{{$board->id}}" method="POST">
             @method('PUT')
             @CSRF
-            <input type="text" name="name" value="{{$board->name}}" />
-            <input type="number" min="1" max="10" name="height" value="{{$board->height}}" />
-            <input type="number" min="1" max="10" name="width" value="{{$board->width}}" />
+            <label> Board Name: </label><input class="home-board-input" type="text" name="name" value="{{$board->name}}" />
+            <label> Board Height: </label><input class="home-board-input" type="number" min="1" max="10" name="height" value="{{$board->height}}" />
+            <label> Board Width: </label><input class="home-board-input" type="number" min="1" max="10" name="width" value="{{$board->width}}" />
             <button type="submit">Save</button>
           </form>
         </div>
