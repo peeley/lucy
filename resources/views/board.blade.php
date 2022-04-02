f2a9e946fc0046469a4d8c4536347a352ff99721
lvs-vong <64504438+lvs-vong@users.noreply.github.com>
2022-04-02 14:52:34 -0700

    added popup dialogs

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -6,8 +6,10 @@
 
   <div id="root" board_id={{ $board_id }}></div>
   <script src="/js/app.js"></script>
-
+  <div id="guidedPopup1"><h1>Press the word you want to say</h1></div>
+  <div id="guidedPopup2"><h1>Press on the sentence bar or the speak button to talk</h1></div>
   <link rel="stylesheet" href="/css/view-transitions.css">
+  <link rel="stylesheet" href="/css/guided-use.css">
   <div class="transition transition-2 is-active"></div>
   <script src="/js/view-transitions.js"></script>
   <script src="/js/guided-use.js"></script>
