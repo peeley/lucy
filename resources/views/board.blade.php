39709f3a45b56de5a94a30a5056833e6fdfd0663
lvs-vong <64504438+lvs-vong@users.noreply.github.com>
2022-04-05 20:23:48 -0700

    added dialogue for popup3

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -8,6 +8,8 @@
   <script src="/js/app.js"></script>
   <div id="guidedPopup1"><h1>Press the word you want to say</h1></div>
   <div id="guidedPopup2"><h1>Press on the sentence bar or the speak button to talk</h1></div>
+  <div id="guidedPopup3"><h1>Note: You can create or edit your own words by pressing and holding down a tile for 3 seconds. <br><br>
+                             (You can close this dialog by pressing anywhere on the screen)</h1></div>
   <link rel="stylesheet" href="/css/view-transitions.css">
   <link rel="stylesheet" href="/css/guided-use.css">
   <div class="transition transition-2 is-active"></div>
