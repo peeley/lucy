d31ae9b449e0f184853586f2fb2ae192a9590f58
lvs-vong <64504438+lvs-vong@users.noreply.github.com>
2022-04-08 13:20:41 -0700

    Separated the edit and delete buttons from board

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,51 +1,58 @@
 <html>
-  <head>
-    <title>Lucy - Home</title>
-      @include('layouts.includes.header')
-      <link rel="stylesheet" href="@if(session('isDark')) /css/home-dark.css @else /css/home.css @endif " />
-      <div class="transition transition-2 is-active"></div>
-      <script src="/js/view-transitions.js"></script>
-  </head>
-  <body>
+
+<head>
+  <title>Lucy - Home</title>
+  @include('layouts.includes.header')
+  <link rel="stylesheet" href="@if(session('isDark')) /css/home-dark.css @else /css/home.css @endif " />
+  <div class="transition transition-2 is-active"></div>
+  <script src="/js/view-transitions.js"></script>
+</head>
+
+<body>
   @include('layouts.includes.darkThemeToggle')
 
 
   <p class="home-welcome-title">Welcome, {{ $name }}.</p>
   <ul class="home-bar">
     <li><button onclick="window.location = '/logout' ">Logout</button></li>
-     <li><button onclick="window.location = '/user-settings' ">Settings</button></li>
+    <li><button onclick="window.location = '/user-settings' ">Settings</button></li>
   </ul>
 
-    <form action="/boards" method="POST">
-      @csrf
-      <button class="home-create-board-button" type="submit">Create Board</button>
-    </form>
-    <ul class="home-boards">
+  <form action="/boards" method="POST">
+    @csrf
+    <button class="home-create-board-button" type="submit">Create Board</button>
+  </form>
+  <ul class="home-boards">
 
     @foreach ($boards as $board)
-      <li class="home-board">
-      <a href="/boards/{{ $board->id }}"> {{ $board->name }} </a>
-      <button onclick="document.getElementById('modal-{{ $board->id }}').style.display='block'"> Edit </button>
-
-      <form action="/boards/{{ $board->id }}" method="POST">
-      @method('DELETE')
-      @csrf
-      <button type="submit">Delete</button>
+    <li class="home-board board-separator">
+      <div>
+        <a href="/boards/{{ $board->id }}"> {{ $board->name }} </a>
+      </div>
+      <div>
+        <form action="/boards/{{ $board->id }}" method="POST">
+          @method('DELETE')
+          @csrf
+          <button type="submit">Delete</button>
         </form>
-        <div id="modal-{{ $board->id }}" style="display: none;">
-          <form action="/boards/{{$board->id}}" method="POST">
-            @method('PUT')
-            @CSRF
-            <label style="white-space: nowrap;"> Board Name: </label><input class="home-board-input" type="text" name="name" value="{{$board->name}}" />
-            <label style="white-space: nowrap;"> Board Height: </label><input class="home-board-input" type="number" min="1" max="10" name="height" value="{{$board->height}}" />
-            <label style="white-space: nowrap;"> Board Width: </label><input class="home-board-input" type="number" min="1" max="10" name="width" value="{{$board->width}}" />
-             <button class="home-board-edit" onclick="document.getElementById('modal-{{ $board->id }}').style.display='none'">Close &times;</button>
+        <button onclick="document.getElementById('modal-{{ $board->id }}').style.display='block'"> Edit </button>
+      </div>
 
-              <button class="home-board-edit" type="submit">Save</button>
-          </form>
-        </div>
-      </li>
+      <div id="modal-{{ $board->id }}" style="display: none;">
+        <form action="/boards/{{$board->id}}" method="POST">
+          @method('PUT')
+          @CSRF
+          <label style="white-space: nowrap;"> Board Name: </label><input class="home-board-input" type="text" name="name" value="{{$board->name}}" />
+          <label style="white-space: nowrap;"> Board Height: </label><input class="home-board-input" type="number" min="1" max="10" name="height" value="{{$board->height}}" />
+          <label style="white-space: nowrap;"> Board Width: </label><input class="home-board-input" type="number" min="1" max="10" name="width" value="{{$board->width}}" />
+          <button class="home-board-edit" onclick="document.getElementById('modal-{{ $board->id }}').style.display='none'">Close &times;</button>
+
+          <button class="home-board-edit" type="submit">Save</button>
+        </form>
+      </div>
+    </li>
     @endforeach
     <ul>
-  </body>
+</body>
+
 </html>
