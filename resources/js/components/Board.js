c2599bb5b325fef63001542f9ce048ba5e5fdfe0
Ren Schuon <64823398+RenSchuon@users.noreply.github.com>
2022-04-28 14:36:40 -0700

    using proper language this time

--- resources/js/components/Board.js
+++ resources/js/components/Board.js
@@ -125,11 +125,7 @@ export class Board extends React.Component {
         const tileType = tile == 'blank' ? 'blank' : (tile.contents ? 'folder' : 'word');
         const tileText = tile.contents ? tile.name : tile.text;
         return <td style={{ "backgroundColor": `${tile.color}`}}
-            if(tileType == "folder"){
-                className="default-tile"
-            } else {
-                className="folder-tile"
-            }
+            className={fileType === 'folder' ? 'folder-tile' : 'default-tile'}
                    onClick={tileType == 'blank'
                             ? () => {}
                             : (tile.contents
