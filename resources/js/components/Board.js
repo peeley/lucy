e35df2435480618936d2d5e534fb54ffed4aea99
Ren Schuon <64823398+RenSchuon@users.noreply.github.com>
2022-04-28 13:34:35 -0700

    fix missing "

--- resources/js/components/Board.js
+++ resources/js/components/Board.js
@@ -125,7 +125,7 @@ export class Board extends React.Component {
         const tileType = tile == 'blank' ? 'blank' : (tile.contents ? 'folder' : 'word');
         const tileText = tile.contents ? tile.name : tile.text;
         return <td style={{ "backgroundColor": `${tile.color}`}}
-            if(tileType == "folder){
+            if(tileType == "folder"){
                 className="default-tile"
             } else {
                 className="folder-tile"
